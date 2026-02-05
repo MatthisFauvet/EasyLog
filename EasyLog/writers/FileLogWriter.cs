a7b41511e97cbb740a59ac35b1b99f66cad19a10
matth <matthisf.etudes@gmail.com>
2026-02-05 11:01:09 +0100

    :memo: Wrote documentation for method

--- EasyLog/writers/FileLogWriter.cs
+++ EasyLog/writers/FileLogWriter.cs
@@ -9,19 +9,62 @@ public class FileLogWriter : ILogWriter
 
     private string _filePath;
 
-    public FileLogWriter(string filePath, string context)
+    private string _context;
+
+    private StreamWriter _streamWriter; 
+
+    
+    /// <summary>
+    /// This class allow user to create text log file 
+    /// </summary>
+    /// <param name="fileDirectory">The destination path for the file</param>
+    /// <param name="context">The context can be a description, a name. It's use to help you find and name log file</param>
+    public FileLogWriter(string fileDirectory, string context)
     {
-        _filePath = filePath;
-        //_fileName = computeFileName(context);
+        _context = context;
+        _filePath = ComputeFilePath(fileDirectory, context);
+        InitFileStream();
     }
     
-    public void write(LogType logType, string message, DateTime timeStamp)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="logType">type of log</param>
+    /// <param name="message">core message : Exception, Message, Information</param>
+    /// <param name="timeStamp">When did the error occured</param>
+    public void write(LogType logType, string message, string timeStamp)
     {
-        throw new NotImplementedException();
+        try
+        {
+            _streamWriter.WriteLine($"[{logType}] - {timeStamp} - {message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
     }
 
-    private void computeFileName(string context)
+    private void InitFileStream()
     {
-        //TODO In the futur this method have to return a String. Return as a string a name for the file.  
+        try
+        {
+            FileStream fs = File.Create(_filePath);
+            _streamWriter = new StreamWriter(fs);
+            _streamWriter.AutoFlush = true;
+        } 
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    private string ComputeFilePath(string fileDirectory, string context)
+    {
+        return Path.Combine(fileDirectory, ComputeFileName(context));
+    }
+
+    private string ComputeFileName(string context)
+    {
+        return context + "-" + DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + ".txt";
     }
 }

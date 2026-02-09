1e03bb26799b2eb60e8236f80d757de94f8d3be3
matth <matthisf.etudes@gmail.com>
2026-02-09 16:04:53 +0100

    :heavy_plus_sign: Created new util class to easily create dictionnary.

---
+++ EasyLog/utils/DictionaryManager.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyLog.utils;
+
+public class DictionaryManager
+{
+    public DictionaryManager()
+    {
+    }
+
+    public static Dictionary<string, string> SingleStringToDictionary(string key, string value)
+    {
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        dictionary.Add(key, value);
+        return dictionary;
+    }
+}
